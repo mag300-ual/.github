28d839068add17a2bcea96c14d9430d10099ae3e
mag300-ual <mag300@inlumine.ual.es>
2026-04-22 16:45:50 +0000

    elimino archivo repetido y hago aceleración PR1

---
+++ PR1/aceleracionPR.py
@@ -0,0 +1,4 @@
+velocidad=float(input("introduce la velocidad en m/s:"))
+aceleracion=float(input("introduce la aceleracion en m/s^2:"))
+tiempo=velocidad/aceleracion
+print("El tiempo que tarda en alcanzar esa velocidad es:", tiempo, "segundos")
